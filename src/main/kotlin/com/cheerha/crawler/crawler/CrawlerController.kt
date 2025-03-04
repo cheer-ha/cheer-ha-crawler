7ea17b1166475bc03507b9397f5741fee01634e5
lcy <plmokn48@daum.net>
2025-03-04 11:49:57 +0900

    ✨ feat: 동기-비동기 테스트 위한 컨트롤러 메서드 구현

--- src/main/kotlin/com/cheerha/crawler/crawler/CrawlerController.kt
+++ src/main/kotlin/com/cheerha/crawler/crawler/CrawlerController.kt
@@ -1,16 +1,54 @@
 package com.cheerha.crawler.crawler
 
+import org.springframework.scheduling.annotation.Async
 import org.springframework.web.bind.annotation.*
+import java.util.concurrent.CompletableFuture
 
 @RestController
 @RequestMapping("/api/crawler")
 class CrawlerController(
-    private val crawler: Crawler
+    private val crawler: List<Crawler>
 ) {
 
-    @GetMapping("/run")
+    //동기 실행
+    @GetMapping("/run-sync")
     fun startCrawling(@RequestParam(defaultValue = "1") pages: Int): String {
-        crawler.crawl(pages)
-        return "크롤링이 시작되었습니다! (최대 $pages 페이지)"
+        val startTime = System.currentTimeMillis()
+        crawler.forEach { crawler ->
+            crawler.crawl(pages)
+        }
+        val totalTime = System.currentTimeMillis() - startTime
+        return "동기 실행 완료! 총 소요 시간: ${totalTime}ms"
     }
+
+    //비동기 멀티스레드 실행
+    @GetMapping("/run-async")
+    fun startCrawlingAsync(@RequestParam(defaultValue = "1") pages: Int): String {
+        val startTime = System.currentTimeMillis()
+        val futures = crawler.map { asyncCrawl(it, pages) }
+        CompletableFuture.allOf(*futures.toTypedArray()).join() //전체 완료 대기
+        val totalTime = System.currentTimeMillis() - startTime
+        return "멀티스레드 실행 완료 총 소요시간: ${totalTime}ms"
+    }
+
+//    //비동기 코루틴 실행
+//    @GetMapping("/run-coroutine")
+//    suspend fun startCrawlingCoroutine(@RequestParam(defaultValue = "1") pages: Int): String {
+//        val startTime = System.currentTimeMillis()
+//        coroutineScope {
+//            crawler.forEach { crawler ->
+//                launch { crawler.crawl(pages) }
+//            }
+//        }
+//        val totalTime = System.currentTimeMillis() - startTime
+//        return "코루틴 실행 완료 총 소요시간: ${totalTime}ms"
+//    }
+
+    @Async
+    fun asyncCrawl(crawler: Crawler, pages: Int): CompletableFuture<Void> {
+        return CompletableFuture.runAsync {
+            crawler.crawl(pages)
+        }
+    }
+
 }
